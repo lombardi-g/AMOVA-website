00f2ce2541c30d561353f147108c95a17ec8a864
lombardi-g <gabriellomba@gmail.com>
2023-11-16 14:51:58 -0300

    #15 Calculator updates

--- ts/watercalculator.ts
+++ ts/watercalculator.ts
@@ -2,27 +2,26 @@ function calculateConsumption(input: number): string | number {
 
     let consumption: number;
     if (input >= 0 && input <= 6) {
-      consumption = 36.47;
-    } else if (input > 6 && input <= 10) {
-      consumption = (input - 6) * 1.44 + 36.47;
-    } else if (input > 10 && input <= 15) {
-      consumption = (input - 10) * 10.21 + 42.23;
-    } else if (input > 15 && input <= 20) {
-      consumption = (input - 15) * 10.93 + 93.28;
-    } else if (input > 20 && input <= 25) {
-      consumption = (input - 20) * 12.28 + 147.93;
-    } else if (input > 25 && input <= 30) {
-      consumption = (input - 25) * 13.70 + 209.33;
-    } else if (input > 30 && input <= 40) {
-      consumption = (input - 30) * 15.00 + 277.83;
-    } else if (input > 40 && input <= 50) {
-      consumption = (input - 40) * 16.52 + 427.83;
-    } else {
-      consumption = (input - 50) * 19.87 + 593.03;
-    }
-  
+      consumption = 38.92;
+  } else if (input > 6 && input <= 10) {
+      consumption = (input - 6) * 1.54 + 38.92;
+  } else if (input > 10 && input <= 15) {
+      consumption = (input - 10) * 10.90 + 45.08;
+  } else if (input > 15 && input <= 20) {
+      consumption = (input - 15) * 11.66 + 99.58;
+  } else if (input > 20 && input <= 25) {
+      consumption = (input - 20) * 13.11 + 157.88;
+  } else if (input > 25 && input <= 30) {
+      consumption = (input - 25) * 14.62 + 223.43;
+  } else if (input > 30 && input <= 40) {
+      consumption = (input - 30) * 16.07 + 296.53;
+  } else if (input > 40 && input <= 50) {
+      consumption = (input - 40) * 17.63 + 457.23;
+  } else {
+      consumption = (input - 50) * 21.21 + 633.53;
+  } 
     return consumption.toFixed(2);
-  }
+}
 
 let aguaInput = document.getElementById('aguaInput') as HTMLInputElement;
 let result = document.getElementById('result') as HTMLSpanElement;
